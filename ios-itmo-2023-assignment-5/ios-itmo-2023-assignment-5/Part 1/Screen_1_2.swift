5a75e91dffb99d587e0be27eae38159c37d734a9
0SNP0 <36269903+0SNP0@users.noreply.github.com>
2023-12-15 02:18:50 +0300

    Screen_1_2

--- ios-itmo-2023-assignment-5/ios-itmo-2023-assignment-5/Part 1/Screen_1_2.swift
+++ ios-itmo-2023-assignment-5/ios-itmo-2023-assignment-5/Part 1/Screen_1_2.swift
@@ -3,9 +3,70 @@ import SwiftUI
 struct Screen_1_2: View {
 
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            EnterPasswordView()
+            Spacer()
+            VKButton()
+        }
     }
 
+}
+
+struct EnterPasswordView: View {
+    @State var password: String = ""
+    var body: some View {
+        VStack(spacing: 0) {
+            Image("LogoVKID")
+                .frame(height: 56)
+            
+            Text("Введите пароль")
+                .font(.system(size: 21))
+                .multilineTextAlignment(.center)
+                .padding(.top, 4)
+            
+            SecureField("Введите пароль", text: $password)
+                .font(.system(size: 16))
+                .background {
+                    Color(red: 242/256, green: 243/256, blue: 245/256)
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.black.opacity(0.12))
+                        }
+                        .cornerRadius(10)
+                        .padding(-12)
+                }
+                .padding(.top, 26)
+                .frame(width: 343)
+            
+            HStack {
+                Button {
+                } label: {
+                    Text("Забыли или не установили пароль?")
+                        .font(.system(size: 18))
+                        .foregroundStyle(Color.blue)
+                        .padding(.top, 22)
+                        .padding(.horizontal, 16)
+                }
+                Spacer()
+            }
+        }
+    }
+}
+
+struct VKButton: View {
+    var body: some View {
+        Button {
+        } label: {
+            ZStack {
+                RoundedRectangle(cornerRadius: 10)
+                Text("Продолжить")
+                    .font(.system(size: 17))
+                    .foregroundColor(.white)
+            }
+        }
+        .padding([.leading, .trailing], 12)
+        .frame(height: 44)
+    }
 }
 
 #Preview {
